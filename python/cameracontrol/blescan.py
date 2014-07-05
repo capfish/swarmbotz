a7abaad16b182ee6f9bf5cc709a0ad575d17406c
Nancy Ouyang <nancy.ouyang@gmail.com>
2014-07-05 17:37:37 -0400

    tested all cases, including unplugged->plugged->no addresses found. ugh edge cases'

--- python/cameracontrol/blescan.py
+++ python/cameracontrol/blescan.py
@@ -2,37 +2,33 @@ import pexpect, time, sys, signal
 
 def closeall(connection):
     isalive = connection.terminate(force=True)
-    print 'process was killed: ', isalive
+    #print 'process was killed: ', isalive
     #hciout.kill(signal.SIGTERM)
     connection.close(force = True)
 
 def gatheradr(conn):
-    print 'Scanning for addresses...'
     start_time = time.time()
-    elapsed_time = 0
+    print 'Scanning for addresses...'
     ble_adrs = set()
-    try: 
+    try:
         for line in conn:
             address = line.strip().split(' ')[0]
+            #print address
             if address != '':
                 ble_adrs.add(address)
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 1: #in seconds
+                closeall(conn)
+                break
     except (pexpect.TIMEOUT):
-        print '!!!!!! spawn timeout'
-        pass
-
-    #print ble_adrs
-    print 'timeout!'
-    closeall(conn)
+        print 'No BLE addresses found! Are peripherals on and reset?'
+        closeall(conn)
     return ble_adrs
 
 def blescan():
     ble_adrs = set()
-    ble_adrs.add('test')
-    try:
-        hciout = pexpect.spawn('hcitool lescan', timeout=1)
-        print 'spawning!'
-    except (pexpect.TIMEOUT):
-        print '!!! uhm spwan timeout'
+    # seems like we cannot catch the timeout here, must be inside gatheradr? 
+    hciout = pexpect.spawn('hcitool lescan', timeout=1)
     i = hciout.expect(['LE Scan ...','File descriptor in bad state',pexpect.TIMEOUT], timeout=1)
     if i == 0:
         ble_adrs = gatheradr(hciout)
@@ -41,24 +37,24 @@ def blescan():
         while c: #code to check if input acceptable
             inp = raw_input('Check if dongle is plugged in. Type "y" to continue, or type "n" to cancel.')
             if inp.lower().startswith('y'):
-                c = False 
                 hciout.terminate()
                 hciout.close()
                 print 'Waiting a second for dongle to initialize'
                 time.sleep(1)
-                hciout = pexpect.spawn('hcitool lescan')
+                hciout = pexpect.spawn('hcitool lescan', timeout=1)
                 j = hciout.expect(['LE Scan ...',pexpect.TIMEOUT], timeout=1)
                 if j == 0:
                     ble_adrs = gatheradr(hciout)
+                    break
                 if j == 1:
                     'Dongle not plugged in.'
                     closeall(hciout)
-            if inp.lower().startswith('n'):
-                c = False 
+                    break
+            elif inp.lower().startswith('n'):
                 closeall(hciout)
+                break
             else:
                 print 'Did not understand command. Try again.'
-                c = True 
     if i == 2:
         print 'Could not scan: ', hciout.after
         closeall(hciout)
@@ -68,11 +64,9 @@ def blescan():
 if __name__ == "__main__":
     ble_adrs = blescan()
     if ble_adrs:
-        print ble_adrs
+        print 'Addresses found: ', ble_adrs
     else:
-        print 'No addresses found'
-    
-
+        print 'Empty set. No addresses found'
 
 
         #self.handle = 'b' #!! this is the TX service on the nRF8001 adafruit breakout with callbackEcho sketch
